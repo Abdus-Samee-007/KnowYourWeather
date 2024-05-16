d61aed812fd9060875b1c6ec57470ac226101e86
Abdus Samee <abdus007samee@gmail.com>
2024-05-16 22:34:25 +0530

    Add files via upload

---
+++ weather-app/weather.js
@@ -0,0 +1,61 @@
+                    // Javascript file for weather app
+      const apikey = "fd90d60150b8ba6ac596387049a353a5";
+      const apiurl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+      const searchbox = document.querySelector(".search input");
+      const searchbtn = document.querySelector(".search button");
+
+      const weather_icon = document.querySelector(".weather-icon")
+
+
+
+      async function checkweather(city) {
+        let response = await fetch(apiurl+`${city}&appid=${apikey}`);
+
+        if(response.status == 404){
+            document.querySelector(".error").style.display= "block"
+            document.querySelector(".weather").style.display= "None"
+        }
+        else{
+
+            var data = await response.json();
+            
+            console.log(data);
+            
+            document.querySelector(".city").innerHTML = data.name;
+            document.querySelector(".temp").innerHTML =
+            Math.round(data.main.temp) + `°C`;
+            document.querySelector(".humidity").innerHTML =
+            data.main.humidity + "%";
+            document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+            
+            if(data.weather[0].main == "Clear"){
+                weather_icon.src ="images/clear.png"
+            }
+            else if(data.weather[0].main == "Clouds"){
+                weather_icon.src ="images/clouds.png"
+            }
+            else if(data.weather[0].main == "Drizzle"){
+                weather_icon.src ="images/drizzle.png"
+            }
+            else if(data.weather[0].main == "Mist"){
+                weather_icon.src ="images/mist.png"
+            }
+            else if(data.weather[0].main == "Rain"){
+                weather_icon.src ="images/rain.png"
+            }
+            else if(data.weather[0].main == "Snow"){
+                weather_icon.src ="images/snow.png"
+            }
+            document.querySelector(".weather").style.display="block";
+            document.querySelector(".error").style.display= "none"
+        }
+            
+        }
+
+      searchbtn.addEventListener("click", function () {
+        checkweather(searchbox.value);
+      });
+
+      checkweather();
+
